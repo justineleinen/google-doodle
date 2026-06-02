1bc660b5fbf0b52e945c41497c65ec02b1518d64
justineleinen <jleinen1@ewu.edu>
2026-06-01 19:56:48 -0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -3,3 +3,57 @@
 
 // GSAP Dev Tools
 GSDevTools.create();
+
+gsap.registerPlugin();
+
+// Ghost G - float up and down + fade in on load
+gsap.from("#big-G", { opacity: 0, duration: 1, ease: "power2.out" });
+gsap.to("#big-G", {
+  y: -12,
+  duration: 2,
+  ease: "sine.inOut",
+  repeat: -1,
+  yoyo: true,
+  delay: 1
+});
+
+// Pumpkins (o, o, e) - wobble
+gsap.to("#o", {
+  rotation: 3,
+  duration: 2,
+  ease: "sine.inOut",
+  repeat: -1,
+  yoyo: true,
+  transformOrigin: "bottom center"
+});
+
+gsap.to("#e", {
+  rotation: -3,
+  duration: 2,
+  ease: "sine.inOut",
+  repeat: -1,
+  yoyo: true,
+  delay: 0.3,
+  transformOrigin: "bottom center"
+});
+
+// Cat G - tail swish
+gsap.to("#g", {
+  rotation: 4,
+  duration: 1.5,
+  ease: "sine.inOut",
+  repeat: -1,
+  yoyo: true,
+  transformOrigin: "bottom center"
+});
+
+// Candle L - flicker
+gsap.to("#l", {
+  opacity: 0.7,
+  scaleX: 1.02,
+  duration: 0.4,
+  ease: "sine.inOut",
+  repeat: -1,
+  yoyo: true,
+  transformOrigin: "bottom center"
+});
